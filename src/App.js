be6a5025603eb7662239569038bbfc8ff18414d5
kimhsr <kimhsr@naver.com>
2023-02-05 02:31:12 +0900

    리액트 반복문(map)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getNumbers() {
+  // 숫자 네 개를 랜덤하게 뽑는 함수
 }
 
-export default App;
+class NumberBaseBall extends Component {
+  state = {
+    result: "",
+    value: "",
+    answer: getNumbers(),
+    tries: [],
+  };
+
+  onSubmitForm = () => {};
+
+  onChageInput = () => {};
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            maxLength={4}
+            value={this.state.value}
+            onChange={this.onChageInput}
+          />
+        </form>
+        <div>시도: {this.state.tries.length}</div>
+        <ul>
+          {["사과","바나나","포도","귤","감","배","밤"].map((v)=>{
+            return (
+              <li>{v}</li>
+            )
+          })}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default NumberBaseBall;
